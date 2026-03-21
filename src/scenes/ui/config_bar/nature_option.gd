f6ae468d6e4ff14da9dc2ca21be3415585b6a32e
IvanVoirol <ivan@voirol.org>
2026-03-21 13:26:38 +0100

    more config

--- src/scenes/ui/config_bar/nature_option.gd
+++ src/scenes/ui/config_bar/nature_option.gd
@@ -1,3 +1,23 @@
+@tool
 extends Node2D
 
-@export var function : Tout.Mode
+@onready var tout_animation: AnimatedSprite2D = %ToutAnimation
+
+@export var nature : Tout.Mode = Tout.Mode.MOVING:
+	set(new_nature):
+		nature = new_nature
+		draw_nature(nature)
+
+
+func _ready() -> void:
+	draw_nature(nature)
+
+
+func draw_nature(new_nature : Tout.Mode) -> void:
+	match new_nature:
+		Tout.Mode.MOVING: tout_animation.animation = "moving"
+		Tout.Mode.BLOCKING: tout_animation.animation = "blocking"
+		Tout.Mode.KILLING: tout_animation.animation = "killing"
+		Tout.Mode.ENDING: tout_animation.animation = "ending"
+		Tout.Mode.DECORING: tout_animation.animation = "decoring"
+		Tout.Mode.COLLECTING: tout_animation.animation = "collecting"
